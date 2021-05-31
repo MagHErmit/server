2505cb0c3bbed1033d5dd7e67dc3f1a1e022f157
MagHermit <maghermit@maghermit.com>
2021-05-31 18:41:54 +1000

    test basic read/write

--- io_cache_test/io_cache_test.c
+++ io_cache_test/io_cache_test.c
@@ -1,16 +1,14 @@
-#include "stdio.h"
 #include "my_sys.h"
-
 
 int main() {
   int fd;
   IO_CACHE cache;
-
-
+  uchar* buff = malloc(sizeof(uchar) * 16);
   fd = my_open("input.txt",O_CREAT | O_RDWR,MYF(MY_WME));
-  init_io_cache(&cache, fd, 4096, SEQ_READ_APPEND, 0,0, MYF(MY_WME));
+  init_io_cache(&cache, fd, 2, SEQ_READ_APPEND, 0,0, MYF(MY_WME));
 
-
+  my_b_read(&cache, buff, 4);
+  my_b_safe_write(&cache, buff, 1);
   end_io_cache(&cache);
   my_close(cache.file, MYF(MY_WME));
 }
