8d0e779eed5f9c7b62db0af0c1ee8a29a9ce6b05
MagHermit <maghermit@maghermit.com>
2021-06-04 17:16:38 +1000

    done two-thread testing target

--- io_cache_test/io_cache_test.c
+++ io_cache_test/io_cache_test.c
@@ -1,16 +1,39 @@
 #include "my_sys.h"
+#include "pthread.h"
+
+
+IO_CACHE cache;
+uchar* buff_from;
+uchar* buff_to;
+
+void* read_to_cache()
+{
+  for (int i= 0; i < 5; ++i)
+    my_b_read(&cache, buff_to, 4);
+  return NULL;
+}
+
+void* write_to_cache()
+{
+  for (int i= 0; i < 5; ++i)
+    my_b_write(&cache, buff_from, 4);
+  return NULL;
+}
 
 int main() {
+  pthread_t thr_read, thr_write;
+
   int fd;
-  IO_CACHE cache;
-  uchar* buff_from = (uchar*)"qwer gfds hgfd sdfg";
-  uchar* buff_to = (uchar*)malloc(sizeof(uchar) * 20);
+  buff_from = (uchar*)"qwer gfds hgfd sdfg";
+  buff_to = (uchar*)malloc(sizeof(uchar) * 20);
   fd = my_open("input.txt",O_CREAT | O_RDWR,MYF(MY_WME));
   init_io_cache(&cache, fd, 2, CBQ_READ_APPEND, 0,0, MYF(MY_WME));
 
-  my_b_write(&cache, buff_from, 6);
-  my_b_read(&cache, buff_to, 3);
-  my_b_read(&cache, buff_to, 3);
+  pthread_create(&thr_write, NULL, write_to_cache, NULL);
+  pthread_create(&thr_read, NULL, read_to_cache, NULL);
+
+  pthread_join(thr_read, NULL);
+  pthread_join(thr_write, NULL);
 
   end_io_cache(&cache);
   my_close(cache.file, MYF(MY_WME));
